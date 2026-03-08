a537a7b96002da3e30909e382823c0b07b918716
Yogesh Shah <yogesh.shah@fliptrendy.com>
2026-03-08 17:58:42 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com' },
       { protocol: 'https', hostname: 'm.media-amazon.com' },
       { protocol: 'https', hostname: '**.cloudfront.net' },
+      { protocol: 'https', hostname: 'cdn.shopify.com' },
     ],
   },
   async redirects() {
